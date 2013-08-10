37d4073ded5cfe2aabbd8649ae4f2cf4df155ee5
Mihai Rotaru <mihai_rotaru@lavabit.com>
2013-08-10 17:16:13 +0100

    fix composer not installing

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -9,3 +9,15 @@ class{'php5::install':}
 class{'mysql::install':}
 class{'wordpress::install':}
 include composer
+
+file { '/vagrant/wordpress/wp-content/themes/my-theme/composer.json':
+  ensure => file,
+  source => '/vagrant/composer.json',
+  require => Class['composer']
+}
+
+exec { 'get_composer_dependencies':
+  command => '/usr/local/bin/composer install',
+  cwd    => '/vagrant/wordpress/wp-content/themes/my-theme',
+  subscribe => File['/vagrant/wordpress/wp-content/themes/my-theme/composer.json']
+}
