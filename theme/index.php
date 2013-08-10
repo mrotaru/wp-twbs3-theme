3f0210c4c1db8d1943976f24af49d7535789b398
Mihai Rotaru <mihai_rotaru@lavabit.com>
2013-08-10 22:06:52 +0100

    add support for navigation menu

--- theme/index.php
+++ theme/index.php
@@ -1,21 +1,6 @@
 <?php get_header();?>
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".nav-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">Project name</a>
-        <div class="nav-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
+    
+    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
 
     <div class="container">
 
