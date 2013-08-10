9a21b760e19c053a080402e55c5c9067a8fe8743
Mihai Rotaru <mihai_rotaru@lavabit.com>
2013-08-10 09:34:17 +0100

    use composer to get kint

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -3,8 +3,9 @@ exec { "apt_update":
   path    => "/usr/bin"
 }
 
-  class{'git::install':}
-  class{'apache2::install':}
-  class{'php5::install':}  
-  class{'mysql::install':}
-  class{'wordpress::install':}
+class{'git::install':}
+class{'apache2::install':}
+class{'php5::install':}  
+class{'mysql::install':}
+class{'wordpress::install':}
+include composer
