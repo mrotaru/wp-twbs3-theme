6c3307d129d5b9064cdef8b487b8d5a68e542b79
Mihai Rotaru <mihai_rotaru@lavabit.com>
2013-08-11 14:01:00 +0100

    nav menu working

--- theme/functions.php
+++ theme/functions.php
@@ -1,4 +1,21 @@
 <?php
+
+function my_css_attributes_filter($var) {
+    if(is_array($var)){
+        $varci= array_intersect($var, array('current-menu-item'));
+        $cmeni = array('current-menu-item');
+        $selava   = array('active');
+        $selavaend = array();
+        $selavaend = str_replace($cmeni, $selava, $varci);
+    }
+    else{
+        $selavaend= '';
+    }
+return $selavaend;
+}
+add_filter('nav_menu_css_class', 'my_css_attributes_filter', 100, 1);
+add_filter('nav_menu_item_id', 'my_css_attributes_filter', 100, 1);
+
 function register_my_menu() {
   register_nav_menu('header-menu',__( 'Header Menu' ));
 }
--- theme/index.php
+++ theme/index.php
@@ -1,6 +1,24 @@
+<?php require '../vendor/raveren/kint/Kint.class.php';?>
+
 <?php get_header();?>
-    
-    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Project name</a>
+    <?php 
+    wp_nav_menu( array( 
+                'theme_location' => 'header-menu',
+                'container_class' => 'nav-collapse collapse',
+                'menu_class' => 'nav navbar-nav' 
+            )
+        );
+    ?>
+    </div>
+</div>
 
     <div class="container">
 
