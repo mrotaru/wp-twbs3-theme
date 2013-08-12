75e7800f379dbb85ba9fb964e0edd47658a05987
Mihai Rotaru <mihai_rotaru@lavabit.com>
2013-08-13 00:03:57 +0100

    excerpt on home page, show category

--- theme/index.php
+++ theme/index.php
@@ -24,12 +24,27 @@
 
       <div class="wp-twbs-wrapper">
 
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+
+                <?php if ( 'post' == get_post_type() ) : ?>
+                <div class="entry-meta">
+                    <?php //posted_on(); ?>
+                </div><!-- .entry-meta -->
+                <?php endif; ?>
+            </header><!-- .entry-header -->
+
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-          <article>
-              <h2><a href="<?php the_permalink(); ?>" title="For More Info on <?php
-              the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-              <?php the_content(); ?>
-        </article>
+                <?php
+                //get_template_part( 'content', get_post_format() );
+                //get_template_part( 'content' );
+                //the_content('');
+                the_excerpt();
+                echo "<p>Posted in ";
+                the_category(', ');
+                echo "</p>";
+                ?>
         <?php endwhile; else: ?>
         <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
         <?php endif; ?>
